5f1d14ec5c6d12d3dcd1926eefcab8c4cfdf1834
Daniel Scherrer <daniel.scherrer@abraxas.ch>
2019-10-03 10:22:57 +0200

    fix compilation and testing issue, update readme.md and add package description for nuget packaging

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # FO.NET
 
-prepare/FO.NET forked this from http://fonet.codeplex.com/
-
-I forked it from him and updated to .NET Core 2.2.0
+- prepare/FO.NET forked this from http://fonet.codeplex.com/
+- Blair0011 forked it from him and updated to .NET Core 2.2.0
+- daniiiol added some bugfixing and pushed it to NuGet (Id: Fonet.Core)
 
 license: Apache2
 
--- src/TestHello/Program.cs
+++ src/TestHello/Program.cs
@@ -1,6 +1,6 @@
-﻿//Apache2, 2017, WinterDev
+﻿//Apache2, 2019, daniiiol
+//Apache2, 2017, WinterDev
 //Apache2, 2009, griffm, FO.NET
-using System.IO;
 using Fonet;
 
 namespace FonetExample
@@ -10,7 +10,7 @@ namespace FonetExample
         static void Main(string[] args)
         {
             FonetDriver driver = FonetDriver.Make();
-            driver.Render("..\\..\\hello.fo", "hello.pdf");
+            driver.Render("hello.fo", "hello.pdf");
         }
     }
 }
